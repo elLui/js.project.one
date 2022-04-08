5c4c10e6fb0706e9990bc7aa970c577288d8fb23
elluis <elluis.codes@gmail.com>
2022-04-08 04:53:36 -0500

    adding event listener for keyboard escape key === close modal
    TODO :: refactor the close_modal into it;s own function per DRY

--- current_working/modal_demonstration/script.js
+++ current_working/modal_demonstration/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
+
+// assign variables to class properties needed for modal/overlay 'hidden' properties
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+
+//
 const close_modal_button = document.querySelector(".close-modal");
 
 
@@ -35,6 +39,28 @@ for (let i = 0; i < open_modal_buttons.length; i++) {
 
 // this function will add the hidden property class back to the affected modal
 close_modal_button.addEventListener('click', function() {
-    modal.classList.add('hidden')
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 })
+
+// this function will allow the user to click on the overlay (background) and have the same effect as clicking the 'X'
+overlay.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+})
+
+document.addEventListener('keydown', function (e) {
+
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            close_modal_button();
+        }
+
+    }
+})
+
+
+// !! TODO :: refactor the close_modal into it's own function per DRY
+
+
 
