fbf88cb3ad26340b4f6f8c8398c50af113927505
elluis <elluis.codes@gmail.com>
2022-04-10 09:33:39 -0500

    created and refactored close_modal() actions - created and refactored open_modal() actions -

--- current_working/modal_demonstration/script.js
+++ current_working/modal_demonstration/script.js
@@ -8,7 +8,17 @@ const overlay = document.querySelector(".overlay");
 //
 const close_modal_button = document.querySelector(".close-modal");
 
+// helper function used to add the 'hidden' property to our modal and overlay
+const close_modal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
+// helper function use to REMOVE 'hidden' property from overlay and modal
+const open_modal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
 // NodeList === A NodeList is a collection of document nodes (element nodes, attribute nodes, and text nodes).
 // HTMLCollection items can be accessed by their name, id, or index number. NodeList items can only be accessed
@@ -31,36 +41,27 @@ for (let i = 0; i < open_modal_buttons.length; i++) {
     open_modal_buttons[i].addEventListener('click', function () {
         console.log('Button click!')
 
-        modal.classList.remove("hidden");
-        overlay.classList.remove("hidden");
+        open_modal();
     })
     console.log(open_modal_buttons[i].textContent);
 }
 
 // this function will add the hidden property class back to the affected modal
 close_modal_button.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    close_modal();
 })
 
 // this function will allow the user to click on the overlay (background) and have the same effect as clicking the 'X'
 overlay.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    close_modal();
 })
 
+// adds the 'Escape' key to a close modal action
 document.addEventListener('keydown', function (e) {
-
     if (e.key === 'Escape') {
         if (!modal.classList.contains('hidden')) {
-            close_modal_button();
+            close_modal();
         }
-
     }
 })
-
-
-// !! TODO :: refactor the close_modal into it's own function per DRY
-
-
 
