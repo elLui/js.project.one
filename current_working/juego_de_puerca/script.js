57f793eafb1ca75753f5266d34ef2b8c2aba8aae
elluis <elluis.codes@gmail.com>
2022-04-12 18:33:13 -0500

    added load_initial_state() :: will reset both scores_0 && add hidden class to the dice element
    next :: add random_number_generator for 1 through 6

--- current_working/juego_de_puerca/script.js
+++ current_working/juego_de_puerca/script.js
@@ -8,12 +8,21 @@ const score0 = document.querySelector('#score--0');
 // ?? have many elements to capture
 const score1 = document.getElementById('score--1');
 
+// dice_element
 const die = document.querySelector('.dice')
 
-score0.textContent = 0;
-score1.textContent = 0;
+const load_initial_state = function () {
+    score0.textContent = 0;
+    score1.textContent = 0;
+    die.classList.add('hidden');
+}
 
-die.classList.add('hidden');
+
+
+
+load_initial_state();
+
+
 
 
 
