8fcf1090a06e51846ceaa7a0c786e4ffead7fa27
elluis <elluis.codes@gmail.com>
2022-04-12 18:42:44 -0500

    added random_dice_generator - generate a psuedo random number between 1 and 6
    next :: addEventListeners for buttons

--- current_working/juego_de_puerca/script.js
+++ current_working/juego_de_puerca/script.js
@@ -11,16 +11,32 @@ const score1 = document.getElementById('score--1');
 // dice_element
 const die = document.querySelector('.dice')
 
+//buttons
+const new_game_button = document.querySelector('.btn--new');
+const roll_dice_button = document.querySelector('.btn--roll');
+const hold_button = document.querySelector('.btn-roll');
+
+
+
 const load_initial_state = function () {
     score0.textContent = 0;
     score1.textContent = 0;
     die.classList.add('hidden');
 }
 
-
-
-
 load_initial_state();
+
+/**
+ * generate value for dice roll
+ * @returns {number} - number between 1 through 6
+ */
+const random_dice_generator = function () {
+    return Math.trunc(Math.random() * 6) + 1;
+}
+
+
+
+console.log(random_dice_generator());
 
 
 
