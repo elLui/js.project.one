794aa83aace9af403ca09acb7e91cde04a8f9fb9
elluis <elluis.codes@gmail.com>
2022-04-12 19:04:21 -0500

    created current_score_0 && current_score_1 - variables to control score
    added roll_dice_button.addEventListener - still working on the functionality of this listener -

--- current_working/juego_de_puerca/script.js
+++ current_working/juego_de_puerca/script.js
@@ -11,13 +11,20 @@ const score1 = document.getElementById('score--1');
 // dice_element
 const die = document.querySelector('.dice')
 
-//buttons
+// buttons
 const new_game_button = document.querySelector('.btn--new');
 const roll_dice_button = document.querySelector('.btn--roll');
 const hold_button = document.querySelector('.btn-roll');
 
+// scores
+const current_score_0 = document.getElementById('current--0');
+const current_score_1 = document.getElementById('current--1');
 
 
+
+/**
+ * resets playing field to initial state
+ */
 const load_initial_state = function () {
     score0.textContent = 0;
     score1.textContent = 0;
@@ -34,10 +41,24 @@ const random_dice_generator = function () {
     return Math.trunc(Math.random() * 6) + 1;
 }
 
+//
 
+/**
+ *
+ */
+roll_dice_button.addEventListener('click', function() {
+    const dice_value = random_dice_generator();
+    die.classList.remove('hidden');
 
-console.log(random_dice_generator());
+    // ?? using the source attribute and template literal to change the image displayed
+    die.src = `./assets/dice-${dice_value}.png`;
+    console.log(dice_value);
 
+    if (dice_value !== 1) {
+
+    }
+
+})
 
 
 
